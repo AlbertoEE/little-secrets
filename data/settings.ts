23eca18cd913a4badc818cdf561ccf8924151d31
albertoee <albertolopezgarciaee@gmail.com>
2024-08-20 23:24:22 +0200

    Add Words

--- data/settings.ts
+++ data/settings.ts
@@ -13,13 +13,430 @@ export interface playerConfiguration {
 
 export const words: word[] = [
   {
-    disciple: "PIÑA",
-    impostor: "MANGO",
+    disciple: "Piña",
+    impostor: "Mango",
     periodist: "You are the Periodist",
   },
   {
-    disciple: "AGUACATE",
-    impostor: "GUACAMOLE",
+    disciple: "Aguacate",
+    impostor: "Guacamole",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Bolos",
+    impostor: "Béisbol",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Koala",
+    impostor: "Oso",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Techno",
+    impostor: "Bachata",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Familia",
+    impostor: "Felicidad",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Tatuaje",
+    impostor: "Maquillaje",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Vela",
+    impostor: "Fuego",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Comprar",
+    impostor: "Pasear",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Vampiro",
+    impostor: "Zombi",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Sobre",
+    impostor: "Carta",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Caramelo",
+    impostor: "Chocolate",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Pistola",
+    impostor: "Bazuca",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Flequillo",
+    impostor: "Patilla",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Pájaro",
+    impostor: "Pato",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Italia",
+    impostor: "Francia",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Poeta",
+    impostor: "Rapero",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Nariz",
+    impostor: "Ojos",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Nocilla®",
+    impostor: "Mermelada",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Amor",
+    impostor: "Corazón",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Zapatero",
+    impostor: "Rajoy",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "SMS",
+    impostor: "WhatsApp®",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Medusa",
+    impostor: "Pescado",
+    periodist: "You are the Periodist",
+  },{
+    disciple: "Sol",
+    impostor: "Luna",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Mar",
+    impostor: "Lago",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Reloj",
+    impostor: "Calendario",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Perro",
+    impostor: "Lobo",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Montaña",
+    impostor: "Colina",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cuchara",
+    impostor: "Tenedor",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Zapato",
+    impostor: "Sandalia",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Río",
+    impostor: "Arroyo",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cielo",
+    impostor: "Nube",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Viento",
+    impostor: "Brisa",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Manzana",
+    impostor: "Pera",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Gato",
+    impostor: "Tigre",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Libro",
+    impostor: "Revista",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Estrella",
+    impostor: "Planeta",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Camisa",
+    impostor: "Blusa",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Montaña",
+    impostor: "Volcán",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Bicicleta",
+    impostor: "Motocicleta",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Carro",
+    impostor: "Coche",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Mesa",
+    impostor: "Escritorio",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Puerta",
+    impostor: "Ventana",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Flor",
+    impostor: "Rosa",
+    periodist: "You are the Periodist",
+  },  {
+    disciple: "León",
+    impostor: "Tigre",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Avión",
+    impostor: "Helicóptero",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Café",
+    impostor: "Té",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Espejo",
+    impostor: "Ventana",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Oro",
+    impostor: "Plata",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Montaña",
+    impostor: "Valle",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Papel",
+    impostor: "Cartón",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Roca",
+    impostor: "Piedra",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Casa",
+    impostor: "Edificio",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Sombrero",
+    impostor: "Gorra",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Playa",
+    impostor: "Costa",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cometa",
+    impostor: "Estrella",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cuerda",
+    impostor: "Hilo",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Jardín",
+    impostor: "Parque",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Tierra",
+    impostor: "Barro",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Llave",
+    impostor: "Candado",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cuchillo",
+    impostor: "Navaja",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Teléfono",
+    impostor: "Radio",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Reloj",
+    impostor: "Despertador",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Plato",
+    impostor: "Vaso",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Pincel",
+    impostor: "Brocha",
+    periodist: "You are the Periodist",
+  },  {
+    disciple: "Guitarra",
+    impostor: "Violín",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Barco",
+    impostor: "Submarino",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Lámpara",
+    impostor: "Linterna",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Sombrero",
+    impostor: "Casco",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Rey",
+    impostor: "Emperador",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Chocolate",
+    impostor: "Cacao",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Caballo",
+    impostor: "Mula",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Florero",
+    impostor: "Maceta",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Montaña",
+    impostor: "Cañón",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Camión",
+    impostor: "Tráiler",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Periódico",
+    impostor: "Revista",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Escalera",
+    impostor: "Ascensor",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Huevo",
+    impostor: "Omlet",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Vaca",
+    impostor: "Buey",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Mesa",
+    impostor: "Tablón",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Teclado",
+    impostor: "Piano",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Hielo",
+    impostor: "Agua",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cine",
+    impostor: "Teatro",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Granja",
+    impostor: "Ranch",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Gafas",
+    impostor: "Lentes",
+    periodist: "You are the Periodist",
+  },
+  {
+    disciple: "Cuchara",
+    impostor: "Cucharón",
     periodist: "You are the Periodist",
   },
 ];
